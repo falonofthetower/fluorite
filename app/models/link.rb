29877ec50dbb60a61c19f76403b218351f77e255
Peter Karth <falonofthetower@gmail.com>
2015-04-05 12:16:36 -0400

    Shorter name length

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,5 +3,5 @@ class Link < ActiveRecord::Base
   has_many :menus, through: :menu_links
 
   validates :url, presence: true
-  validates :name, presence: true, length: { minimum: 5 }
+  validates :name, presence: true, length: { minimum: 3 }
 end
